88f26f13a5598a1fe5cdb7667a2b8740f754c21a
CristianCadena <cristiancadena1927@gmail.com>
2022-02-23 21:57:13 -0500

    Finish ES8

---
+++ src/es7/index.js
@@ -0,0 +1,19 @@
+// Que es Index: Es la Convension 
+
+//Buscar un elmento en nuestro areglo.
+
+let numbers = [1,2,3,7,8];
+
+if(numbers.includes(7)) {
+    console.log('Si esta el valor 7! :)');
+} else {
+    console.log('No esta el valor 7! :(');
+} 
+
+//Elevar a la potencia.
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log (result);
