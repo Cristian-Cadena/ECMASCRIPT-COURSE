3006cb8a6e219c6b678d6ff3184336439db93a2d
CristianCadena <cristiancadena1927@gmail.com>
2022-02-24 21:10:15 -0500

    Detalles ES7

--- src/es7/index.js
+++ src/es7/index.js
@@ -1,13 +1,13 @@
 // Que es Index: Es la Convension 
 
-//Buscar un elmento en nuestro areglo.
+//Buscar un elemento en nuestro areglo.
 
 let numbers = [1,2,3,7,8];
 
 if(numbers.includes(7)) {
-    console.log('Si esta el valor 7! :)');
+    console.log('Si esta el valor! :)');
 } else {
-    console.log('No esta el valor 7! :(');
+    console.log('No esta el valor! :(');
 } 
 
 //Elevar a la potencia.
